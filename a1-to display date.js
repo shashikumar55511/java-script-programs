396a1068dc9b3561fa6f98753f794076c7c16358
shashi kumar <103835652+shashikumar55511@users.noreply.github.com>
2022-11-10 07:17:42 +0530

    Create a1-to display date.js

---
+++ a1-to display date.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+
+    <body>
+        <h2>shashi kumar</h2>
+        <button type="button" 
+        onclick="document.getElementById('demo').innerHTML= Date()">shashi</button>
+        <div id="demo"></div>
+       </body>
+</html>
